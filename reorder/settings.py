774d8fe48f38eedf626da71e85e8eb80b58c107f
NgatiaJeffers <jeffersongakuya@gmail.com>
2021-08-11 19:47:01 +0300

    Second commit

--- reorder/settings.py
+++ reorder/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'reorder.wsgi.application'
 #     }
 # }
 
-prod_db = dj_database_url.config(conn_max_age=500)
+prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 
