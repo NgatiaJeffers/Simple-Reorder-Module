9b9bc87dbcc61fdbb2b30e53614cdad6e137ad60
NgatiaJeffers <jeffersongakuya@gmail.com>
2021-08-11 14:26:20 +0300

    Create dispatch order page

--- order/urls.py
+++ order/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.item_list, name='items'),
+    path('list', views.order_list, name='order_list'),
 ]
--- order/views.py
+++ order/views.py
@@ -9,3 +9,20 @@ def item_list(request):
         "item_list":item_list,
     }
     return render(request, "index.html", context)
+
+def send_order(id):
+    item = Items.objects.get(id = id)
+    order = Order.objects.create(item_id=item)
+
+    return redirect("items")
+
+def order_list(request):
+    dispatched_order = Order.objects.filter(dispatch=True)
+    pending_order = Order.objects.filter(dispatch=False)
+
+    context = {
+        "dispatched_order":dispatched_order,
+        "pending_orders":list(pending_order),
+    }
+
+    return render(request, "order.html", context)
