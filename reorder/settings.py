8c346e2cf5cf6c9b50ea357dd0faf75a5236ce2b
NgatiaJeffers <jeffersongakuya@gmail.com>
2021-08-11 19:57:53 +0300

    Second commit

--- reorder/settings.py
+++ reorder/settings.py
@@ -92,11 +92,14 @@ WSGI_APPLICATION = 'reorder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Access',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
