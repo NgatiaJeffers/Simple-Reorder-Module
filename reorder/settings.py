1a1e8ed686646ea7df5a61fe2c997e2b07e88107
NgatiaJeffers <jeffersongakuya@gmail.com>
2021-08-11 19:52:00 +0300

    Second commit

--- reorder/settings.py
+++ reorder/settings.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'reorder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
